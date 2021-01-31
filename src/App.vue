e39f4ecb840ed5cf9de31052b54af9833849cc80
Benjamin Janssens <benji.janssens@gmail.com>
2021-01-31 15:50:03 +0100

    💄 Added Bulma

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <b-button>This is a Bulma button</b-button>
   </div>
 </template>
 
