9fa3d3a95d7d75a26dd738f2cc9e54b5f3a78c7b
Benjamin Janssens <benji.janssens@gmail.com>
2021-02-01 19:44:54 +0100

    📱 Improved responsiveness

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,59 @@
 <template>
   <div id="app">
     <div id="content">
-      <div class="box mx-6 my-4">
-        <b-upload
-          drag-drop
-          accept="image/png"
-          @input="uploadImage"
-          expanded
-          v-if="!imageName"
-        >
-          <section class="section">
-            <div class="content has-text-centered">
-              <p>
-                <b-icon icon="upload" size="is-large"> </b-icon>
-              </p>
-              <p>Drop your PNG file here or click here to upload</p>
-            </div>
-          </section>
-        </b-upload>
-        <div v-else>
-          <div class="columns is-centered is-vcentered has-text-centered">
-            <div class="column is-4">
-              <img :src="imageToTrim" />
-            </div>
-            <div class="column is-narrow">
-              <b-icon icon="arrow-right-bold" size="is-large" />
-            </div>
-            <div class="column is-4">
-              <img v-if="imageTrimmed" :src="imageTrimmed" />
-              <b-skeleton v-else height="100px"></b-skeleton>
-            </div>
-          </div>
-          <div class="columns is-centered is-vcentered">
-            <div class="column is-9">
-              <div class="buttons">
-                <b-button
-                  type="is-primary"
-                  expanded
-                  :loading="!Boolean(imageTrimmed)"
-                  tag="a"
-                  :href="imageTrimmed"
-                  :download="`trimpng_${imageName}`"
-                  >Download</b-button
-                >
-                <b-button type="is-danger" expanded @click="resetImage"
-                  >Back</b-button
-                >
+      <div class="columns is-centered is-vcentered is-mobile">
+        <div class="column is-12-mobile is-9-tablet is-6-desktop">
+          <div class="box mx-6 my-4">
+            <b-upload
+              drag-drop
+              accept="image/png"
+              @input="uploadImage"
+              expanded
+              v-if="!imageName"
+            >
+              <section class="section">
+                <div class="content has-text-centered">
+                  <p>
+                    <b-icon icon="upload" size="is-large"> </b-icon>
+                  </p>
+                  <p>Drop your PNG file here or click here to upload</p>
+                </div>
+              </section>
+            </b-upload>
+            <div v-else>
+              <div class="columns is-centered is-vcentered has-text-centered">
+                <div class="column is-4">
+                  <img :src="imageToTrim" />
+                </div>
+                <div class="column is-narrow">
+                  <b-icon
+                    icon="arrow-right-bold"
+                    size="is-large"
+                    id="result-icon"
+                  />
+                </div>
+                <div class="column is-4">
+                  <img v-if="imageTrimmed" :src="imageTrimmed" />
+                  <b-skeleton v-else height="100px"></b-skeleton>
+                </div>
+              </div>
+              <div class="columns is-centered is-vcentered">
+                <div class="column is-12-mobile is-9-tablet is-6-desktop">
+                  <div class="buttons">
+                    <b-button
+                      type="is-primary"
+                      expanded
+                      :loading="!Boolean(imageTrimmed)"
+                      tag="a"
+                      :href="imageTrimmed"
+                      :download="`trimpng_${imageName}`"
+                      >Download</b-button
+                    >
+                    <b-button type="is-danger" expanded @click="resetImage"
+                      >Back</b-button
+                    >
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -104,7 +112,8 @@ export default class App extends Vue {
 
 <style lang="scss">
 html,
-body {
+body,
+.columns {
   height: 100%;
 }
 
@@ -116,19 +125,17 @@ body {
 
 #content {
   flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.box {
-  width: 50%;
 }
 
 img {
   border: 1px dashed #b5b5b5;
   border-radius: 6px;
-  height: 100px !important;
+  max-height: 100px !important;
+}
+
+@media only screen and (max-width: 768px) {
+  #result-icon {
+    transform: rotate(90deg);
+  }
 }
 </style>
